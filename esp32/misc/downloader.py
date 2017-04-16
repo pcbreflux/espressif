f83f3f7ed7a3de06c249516e1c2f95b8bbbbde25
pcbreflux <pcbreflux@gmail.com>
2017-04-16 22:39:50 +0200

    giveaway extractor

--- esp32/misc/downloader.py
+++ esp32/misc/downloader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#based on the scrip from https://github.com/egbertbouman/youtube-comment-downloader
+#based on the script from https://github.com/egbertbouman/youtube-comment-downloader
 #
 
 import os
