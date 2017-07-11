607148ef7732ddadad392bdedf5b5a3626546c2c
pcbreflux <pcbreflux@gmail.com>
2017-07-11 16:26:24 +0200

    ESP32_thermo

--- esp32/arduino/sketchbook/ESP32_int_temp_sensor/ESP32_int_temp_sensor.ino
+++ esp32/arduino/sketchbook/ESP32_int_temp_sensor/ESP32_int_temp_sensor.ino
@@ -1,10 +1,13 @@
 
+#include <time.h>
+#include <sys/time.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 uint8_t temprature_sens_read();
-uint8_t g_phyFuns;
+//uint8_t g_phyFuns;
 
 #ifdef __cplusplus
 }
@@ -13,12 +16,22 @@ uint8_t g_phyFuns;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+    tzset();
 }
 
 uint8_t temp_farenheit;
 float temp_celsius;
+char strftime_buf[64];
+time_t now = 0;
+struct tm timeinfo;
+char buf[256];
+
 
 void loop() {
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    sprintf(buf,"scan start %02d:%02d:%02d ",timeinfo.tm_hour,timeinfo.tm_min,timeinfo.tm_sec);
+    Serial.print (buf);
 
     temp_farenheit= temprature_sens_read();
     temp_celsius = ( temp_farenheit - 32 ) / 1.8;
@@ -28,4 +41,5 @@ void loop() {
     Serial.print(temp_celsius);
     Serial.println("°C");
     delay(1000);
+    now++;
 }
