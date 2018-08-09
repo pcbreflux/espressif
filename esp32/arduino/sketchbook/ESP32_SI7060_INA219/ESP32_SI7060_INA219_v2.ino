4ed8f07152f739644d8c72a557daf4f637765683
pcbreflux <pcbreflux@gmail.com>
2018-08-09 16:48:51 +0200

    ESP32_SI7060_INA219

--- esp32/arduino/sketchbook/ESP32_SI7060_INA219/ESP32_SI7060_INA219_v2.ino
+++ esp32/arduino/sketchbook/ESP32_SI7060_INA219/ESP32_SI7060_INA219_v2.ino
@@ -1,3 +1,17 @@
+/* Copyright (c) 2018 pcbreflux. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. *
+ */
 #include <U8g2lib.h>
 #include <INA219.h>
 #include "i2c_SI7060.h"
